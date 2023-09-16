0f3701e1d29a27a7975374ddb540b9563ab20b92
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-09-16 20:13:26 -0300

    chore: cria projeto spring
    -- API Jogadoresetimes

---
+++ jogadoresetimes/src/main/java/com/airel/jogadoresetimes/JogadoresetimesApplication.java
@@ -0,0 +1,13 @@
+package com.airel.jogadoresetimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogadoresetimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogadoresetimesApplication.class, args);
+	}
+
+}
---
+++ jogadoresetimes/src/test/java/com/airel/jogadoresetimes/JogadoresetimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airel.jogadoresetimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogadoresetimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
