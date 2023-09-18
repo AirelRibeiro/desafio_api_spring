c579bca0d5754e295cd81c2270cc7b04fea725df
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-09-18 20:07:11 -0300

    refactor: retira retorno
    -- para cumprir requisito do desafio

--- jogadoresetimes/src/main/java/com/airel/jogadoresetimes/controllers/JogadorController.java
+++ jogadoresetimes/src/main/java/com/airel/jogadoresetimes/controllers/JogadorController.java
@@ -3,6 +3,7 @@ package com.airel.jogadoresetimes.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +14,18 @@ import com.airel.jogadoresetimes.entities.Jogador;
 import com.airel.jogadoresetimes.services.JogadorService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class JogadorController {
 
   @Autowired
   JogadorService jogadorService;
 
   @PostMapping("/jogador")
-  public String adicionarJogador(@RequestBody Jogador jogador) {
+  public void adicionarJogador(@RequestBody Jogador jogador) {
     try {
-      String message = jogadorService.adicionarJogador(jogador);
-      return message;
+      jogadorService.adicionarJogador(jogador);
     } catch (Exception e) {
-      return e.getMessage();
+      throw e;
     }
   }
 
